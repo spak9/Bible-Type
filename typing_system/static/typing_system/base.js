bb60405a62789103165ff1c6ef4033abc5649e58
Steven Pak <spak9@gmu.edu>
2020-12-26 12:06:00 -0500

    span words

--- typing_system/static/typing_system/base.js
+++ typing_system/static/typing_system/base.js
@@ -1,6 +1,8 @@
 let type_area = document.querySelector(".type-area");
 let verse = "For God so loved the world, that he gave his only Son, that whoever believes in him should not perish but have eternal life.";
+let verse_words = [];
 let words = [];
+let x = 0;
 
 /*
     tokenize a given verse based on " ", that is
@@ -9,13 +11,44 @@ let words = [];
 function tokenizeVerse() {
     let word_array = verse.split(" ").forEach(function(elem) {
         // our span
-        const word = document.createElement("span");
-        const text = document.createTextNode(elem);
-        word.appendChild(text);
+        let word = document.createElement("span");
+        // span's text = elem
+        word.innerHTML = elem;
+        word.classList.add("word")
         type_area.appendChild(word);
+        words.push(word);
         });
 }
 
-//
+/*
+    Tokenize a given list of words based on "", that is
+    by the character, resulting in a mapping of
+    <span> <--> characters
+*/
+function tokenizeWords(i) {
+    let word_text = words[i].textContent; // string
+    word_text.split("").forEach(function(elem) {
+        // span for letter
+        let letter = document.createElement("span");
+        letter.innerHTML = elem;
+        words[i].appendChild(letter);
+    });
+}
+
+verse.split(" ").forEach(function(elem) {
+    verse_words.push(elem);
+})
 
 tokenizeVerse();
+let i;
+for (i = 0; i < words.length; i++) {
+    tokenizeWords(i);
+    words[i].innerHTML = "";
+}
+// alert(words[0].textContent.length);
+// let letter_span = document.createElement("span");
+// letter_span.innerHTML = words[0].textContent[0]; // [f]or
+// let letter_span2 = document.createElement("span");
+// letter_span2.innerHTML = words[0].textContent[1]; // [f]or
+// words[0].appendChild(letter_span);
+// words[0].appendChild(letter_span2);
