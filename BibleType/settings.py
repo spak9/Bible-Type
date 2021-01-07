7b097b5f9f5cd714d565d687f59981b3dd82dcf7
Steven <spak9@gmu.edu>
2021-01-07 02:13:33 -0500

    changed secret keys

--- BibleType/settings.py
+++ BibleType/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^c6+dsoil_m!wu381&mbubs)+s%3yl13nrbg)=_f_)sva^&hgk'
-# SECRET_KEY = os.environ.get('BIBLETYPE_SECRET_KEY')
+
+SECRET_KEY = os.environ.get('BIBLETYPE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
