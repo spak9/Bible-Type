d333d4130b9216abb6179f372c9db2cb939adabc
Steven <spak9@gmu.edu>
2021-01-07 01:56:53 -0500

    added django-on-heroku

--- BibleType/settings.py
+++ BibleType/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# django on heroku
+django_on_heroku.settings(locals())
