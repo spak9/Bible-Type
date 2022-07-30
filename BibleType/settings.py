e9af643042883f0ef4a26eeb9c9f993c839bf044
Steven Pak <spak9@gmu.edu>
2022-07-29 20:06:38 -0400

    Updated SECRET_KEY - need to re-do this step..

--- BibleType/settings.py
+++ BibleType/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('BIBLETYPE_SECRET_KEY')
+# SECRET_KEY = os.environ.get('BIBLETYPE_SECRET_KEY')
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
