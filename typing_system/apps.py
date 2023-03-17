f158b440269b4ee03bb7573d60cc172de7ea2f62
Steven Pak <42177856+spak9@users.noreply.github.com>
2023-03-16 22:45:34 -0400

    Refactor to express (#4)
    * Moving everything to an OLD folder
    
    * Moved the project from basic Django a very basic Express server
    
    * Got rid of 'OLD' Django files and have fully moved to express

--- typing_system/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TypingSystemConfig(AppConfig):
-    name = 'typing_system'
--- typing_system/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    # index view
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-]
