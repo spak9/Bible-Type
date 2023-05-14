37c8a8c993d8c8512b5871f61826c82e62938e16
Steven Pak <spak9@gmu.edu>
2023-05-14 15:03:12 -0400

    Added smui and set up basic CSS themes, need to slowly port over code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+	import Button, { Icon } from '@smui/button';
+	let clicks = 0;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button on:click={() => clicks++}>
+	<Icon class="material-icons">favorite</Icon>Click Me {clicks}
+</Button>
