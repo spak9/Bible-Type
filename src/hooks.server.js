c49ab8798c654bb5a6f9b525d164794a3c48dd99
Steven Pak <spak9@gmu.edu>
2023-08-15 21:39:58 -0500

    Updated :root css styles to updatre Material typography font for all design components. Begun changing +page.svelte for /account to account for this

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -3,7 +3,7 @@ import { serializeNonPOJOs } from '$lib/utils';
 
 
 export async function handle({ event, resolve }) {
-    console.log(`+hooks.server.js - Incoming request from ${event.url.pathname}`)
+    console.log(`+hooks.server.js - Incoming Request from ${event.url.pathname}`)
     
     // Add PocketBase instance to "locals" kv store
     event.locals.pb = new PocketBase("http://127.0.0.1:8090");
@@ -15,7 +15,6 @@ export async function handle({ event, resolve }) {
     // 1. User is logged IN
     if (event.locals.pb.authStore.isValid) {
         event.locals.user = serializeNonPOJOs(event.locals.pb.authStore.model);
-        console.log(`+hooks.server.js - AuthStore is valid - ${event.locals.user}`);
     }
     // 2. User is logged OUT
     else {
@@ -28,6 +27,6 @@ export async function handle({ event, resolve }) {
     // Update HTTP Response with some cookies - 
     // Setting "pb_auth" cookie
     res.headers.append("set-cookie", event.locals.pb.authStore.exportToCookie())
-
+    console.log(`+hooks.server.js - Returning Response from ${event.url.pathname}`)
     return res;
 }
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,28 +1,64 @@
 <script>
+    import Textfield from '@smui/textfield';
+
     console.log("+page.svelte at /account");
 
     export let data;
+
+    let reg_username = "";
+    let reg_password = "";
+    let reg_confirm_password = "";
+
+    let login_username = "";
+    let login_password = "";
+
 </script>
 
 {#if data.user}
     <h2>User is logged in</h2>
 {:else}
     <div class="account-form-wrapper">
+        <!-- Wrap in HTML form for Svelte form actions -->
         <form action="?/register" method="POST" class="account-form">
             <h2>Register</h2>
-            <input type="email" name="email" />
-            <input type="password" name="password" />
-            <input type="password" name="confirm-password" />
+            <div>
+                <Textfield 
+                required
+                label="Username" 
+                bind:value={reg_username}></Textfield>
+            </div>
+            <div>
+                <Textfield 
+                required
+                label="Password" 
+                bind:value={reg_password}></Textfield>
+            </div>
+            <div>
+                <Textfield 
+                required
+                label="Confirm Password"
+                bind:value={reg_confirm_password}></Textfield>
+            </div>
             <button type="submit">Register</button>
         </form>
+        
         <form action="?/login" method="POST" class="account-form">
             <h2>Login</h2>
-            <input type="email" name="email" />
-            <input type="password" name="password" />
+            <div>
+                <Textfield 
+                required
+                label="Username" 
+                bind:value={login_username}></Textfield>
+            </div>
+            <div>
+                <Textfield 
+                required
+                label="Password" 
+                bind:value={login_password}></Textfield>
+            </div>
             <button type="submit">Login</button>
         </form>
     </div>
-
 {/if}
 
 
@@ -30,7 +66,7 @@
     .account-form-wrapper {
         display: flex;
         flex-direction: row;
-        justify-content: space-evenly;
+        justify-content: space-around;
         align-items: center;
         height: 100%
     }
@@ -41,4 +77,8 @@
         align-items: center;
         height: 100%;
     }
+    form.account-form > div {
+        margin: 16px;
+    }
+
 </style>
