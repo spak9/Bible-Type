2881522a70b5d58fb5b61dc59ff396308b2bad66
Steven Pak <spak9@gmu.edu>
2023-10-29 21:23:33 -0400

    Added  AppShell to outer layout.svelte - need to figure out how AppShell is using flex for it's stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
   import "../app.postcss";
+  import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+
+<AppShell>
+  <svelte:fragment slot="header">Header</svelte:fragment>
+  <slot />
+  <svelte:fragment slot="footer">Footer</svelte:fragment>
+</AppShell>
