b4f90b6ea2221905c3defcccf144a2ce5df40c70
Steven Pak <spak9@gmu.edu>
2023-10-31 18:37:39 -0400

    Moving to FA version 5 (also with the cdn, no kit). Fixing the Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,22 +2,30 @@
   import "../app.postcss";
   import { AppShell } from '@skeletonlabs/skeleton';
 
-  let currentVariant = "variant-filled"
+  export let data;
 </script>
 
 
+<!-- AppShell == Header + page slot + Footer -->
 <AppShell>
   <svelte:fragment slot="header">
     <nav class="flex items-center my-8">
       <a href="/" class="btn">
         Bible Type
       </a>
-      <a href="/about" class="btn-icon">
-        <i class="fa-regular fa-user"></i>
+      <a href="/about" class="btn-icon ml-auto">
+        <i class="fas fa-info"></i>  
       </a>
-
+      <a href="/account" class="btn-icon">
+        <i class="fas fa-user"></i> 
+      </a>
     </nav>
   </svelte:fragment>
   <slot />
-  <svelte:fragment slot="footer">Footer</svelte:fragment>
+  {#if data?.user}
+    <form method="POST" action="/logout" class="">
+      <i class="fas fa-sign-out-alt"></i>
+    </form>
+  {/if}
+  <div slot="footer" class="my-8">Footer</div>
 </AppShell>
