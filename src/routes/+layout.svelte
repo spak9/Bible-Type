1fd99337190c803d047cf63482adbc64c7a9c6b6
Steven Pak <spak9@gmu.edu>
2023-10-30 22:29:13 -0400

    Adding old navbar and styles to AppShell -- figuring out fontawesome and why the icons won't appear with Skeleton Button components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
   import "../app.postcss";
   import { AppShell } from '@skeletonlabs/skeleton';
+
+  let currentVariant = "variant-filled"
 </script>
 
 
 <AppShell>
-  <svelte:fragment slot="header">Header</svelte:fragment>
+  <svelte:fragment slot="header">
+    <nav class="flex items-center my-8">
+      <a href="/" class="btn">
+        Bible Type
+      </a>
+      <a href="/about" class="btn-icon">
+        <i class="fa-regular fa-user"></i>
+      </a>
+
+    </nav>
+  </svelte:fragment>
   <slot />
   <svelte:fragment slot="footer">Footer</svelte:fragment>
 </AppShell>
