528526c9181194cc5d53a25ca42594453d6315ff
Steven Pak <spak9@gmu.edu>
2023-10-31 21:02:10 -0400

    Navbar finished

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,7 @@
 
 <!-- AppShell == Header + page slot + Footer -->
 <AppShell>
+  <!-- Header/Navbar -->
   <svelte:fragment slot="header">
     <nav class="flex items-center my-8">
       <a href="/" class="btn">
@@ -19,13 +20,17 @@
       <a href="/account" class="btn-icon">
         <i class="fas fa-user"></i> 
       </a>
+      {#if data.user}
+        <form method="POST" action="/logout" class="">
+          <i class="fas fa-sign-out-alt"></i>
+        </form>
+      {/if}
     </nav>
   </svelte:fragment>
+
+  <!-- Page Slot -->
   <slot />
-  {#if data?.user}
-    <form method="POST" action="/logout" class="">
-      <i class="fas fa-sign-out-alt"></i>
-    </form>
-  {/if}
-  <div slot="footer" class="my-8">Footer</div>
+
+  <!-- Footer -->
+  <div slot="footer" class="my-4 text-center">© Steven Pak 2023</div>
 </AppShell>
